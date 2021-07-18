803f7c98c89ca6d6f290b9a07307e76601c9dc1b
kadirmeert <kadirmert.34@hotmail.com>
2021-07-18 16:34:24 +0300

    Initial Commit

---
+++ stackatada-stack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  stackatada-stack
+//
+//  Created by MERT on 18.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
